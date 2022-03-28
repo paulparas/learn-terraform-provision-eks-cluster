9c085d533cacf02d65e837ffb098687ddbb105d3
paulparas <paul.paraspaul@gmail.com>
2022-03-28 15:56:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,11 @@
 terraform {
   backend "remote" {
     # The name of your Terraform Cloud organization.
-    organization = "example-organization"
+    organization = "k8s-on-eks"
 
     # The name of the Terraform Cloud workspace to store Terraform state files in.
     workspaces {
-      name = "example-workspace"
+      name = "k8s-on-eks"
     }
   }
 }
